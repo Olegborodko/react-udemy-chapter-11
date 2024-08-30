c80e129b72104b1c3b0ba425b5e869acd710e1d5
borodkoleg <borodkoleg@gmail.com>
2024-08-30 11:34:35 +0300

    side effects, localStorage, add/remove objects to array code example (map, find, filter, JSON.parse)

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,26 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import { sortPlacesByDistance } from './loc.js';
 
+// CORRECT
+const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+const storedPlaces = storedIds.map(id => AVAILABLE_PLACES.find((place) => place.id === id));
+// если переместить этот код в App то будет теряться производительность,
+// так как этот код нужно запускать только первый раз
+// =======
+
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  // DON'T CORRECT
+  // useEffect(() => {
+  //   const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+  //   const storedPlaces = storedIds.map(id => AVAILABLE_PLACES.find((place) => place.id === id));
+
+  //   setPickedPlaces(storedPlaces);
+  // }, []);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -42,6 +57,11 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem('selectedPlaces', JSON.stringify([id, ...storedIds]));
+    }
   }
 
   function handleRemovePlace() {
@@ -49,6 +69,11 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    localStorage.setItem('selectedPlaces', JSON.stringify(storedIds.filter(
+      (id) => id !== selectedPlace.current
+    )));
   }
 
   return (
